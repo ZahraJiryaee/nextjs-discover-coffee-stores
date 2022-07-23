8c8a6c5d254f2d4d577243e0198298a44be3dfd9
Zahra Jiryaee <zahrajiryaee77@gmail.com>
2022-07-23 16:52:24 +0430

    apply hero image -- improve banner and home style

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 import Banner from "../components/banner";
 
@@ -21,6 +22,9 @@ export default function Home() {
           buttonText="View stores nearby"
           handleOnClick={handleOnBannerButtonClick}
         />
+        <div className={styles.heroImage}>
+          <Image src={"/static/hero-image.png"} width={700} height={400} />
+        </div>
       </main>
     </div>
   );
