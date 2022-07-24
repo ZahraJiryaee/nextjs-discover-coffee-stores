5727914bdc4a4c5504ddee61c1c0a1473ad7fbaa
z.jiryaee <zahrajiryaee77@gmail.com>
2022-07-24 13:12:29 +0430

    refactor getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,14 +1,24 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 
 import coffeeStoresData from "../../data/coffee-stores.json";
 
 /* Server Side */
 export function getStaticPaths() {
   console.log("getStaticPaths");
+
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: { id: coffeeStore.id.toString() },
+    };
+  });
+
+  console.log("paths:", paths);
+
   return {
-    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    paths,
     fallback: true,
   };
 }
@@ -36,14 +46,19 @@ const CoffeeStore = (props) => {
     return <div>Loading ...</div>;
   }
 
+  const { address, name, neighbourhood } = props.coffeeStore;
+
   return (
     <div>
-      <h1>Coffee Store Page {router.query.id}</h1>
+      <Head>
+        <title>{name}</title>
+      </Head>
       <Link href="/">
         <a>back to home</a>
       </Link>
-      <p>{props.coffeeStore.address}</p>
-      <p>{props.coffeeStore.name}</p>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
     </div>
   );
 };
