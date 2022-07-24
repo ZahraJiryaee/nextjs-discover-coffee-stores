e0d569fca74153b4fcb2f2a930a62ef330664ca0
z.jiryaee <zahrajiryaee77@gmail.com>
2022-07-24 13:04:40 +0430

    Implement Get Static Props and Get Static Paths in Next.js

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -2,15 +2,48 @@ import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+/* Server Side */
+export function getStaticPaths() {
+  console.log("getStaticPaths");
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  console.log("getStaticProps- params:", params);
+
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(
+        (coffeeStore) => coffeeStore.id.toString() === params.id
+      ),
+    }, // will be passed to the page component as props
+  };
+}
+
+/* Client Side */
+const CoffeeStore = (props) => {
   const router = useRouter();
+  console.log("router:", router);
+  console.log("props - id:", props);
+
+  if (router.isFallback) {
+    return <div>Loading ...</div>;
+  }
 
   return (
     <div>
-      <h1>ji {router.query.id}</h1>
+      <h1>Coffee Store Page {router.query.id}</h1>
       <Link href="/">
         <a>back to home</a>
       </Link>
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
     </div>
   );
 };
